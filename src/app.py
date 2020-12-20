1d16a1ebffaf479170881c38e43836ec7c67b6bd
Saiprasad Balasubramanian <sai.cosmo@gmail.com>
2020-12-20 19:32:50 +0530

    Changed local env port

--- src/app.py
+++ src/app.py
@@ -31,4 +31,4 @@ if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'build':
         freezer.freeze()
     else:
-        app.run(host='0.0.0.0', port=5000)
+        app.run(host='0.0.0.0', port=8001)
