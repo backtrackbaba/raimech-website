57ef3fb93089a57fdf57a21ab75f5d520f980309
Saiprasad Balasubramanian <sai.cosmo@gmail.com>
2020-12-20 19:23:57 +0530

    Initialized Flask with Jumpstart and Flask-Frozen

---
+++ src/app.py
@@ -0,0 +1,34 @@
+import sys
+
+from flask import Flask, render_template
+from flask_frozen import Freezer
+
+app = Flask(__name__)
+freezer = Freezer(app)
+
+
+@app.route('/')
+def home():
+    return render_template('pages/index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('pages/about.html')
+
+
+@app.route('/careers')
+def careers():
+    return render_template('pages/careers.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('pages/contact.html')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == 'build':
+        freezer.freeze()
+    else:
+        app.run(host='0.0.0.0', port=5000)
